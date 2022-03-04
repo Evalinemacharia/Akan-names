5c63bbf684b423add67a67bd2030adb443ca7ee5
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-04 16:18:37 +0300

    add the submit button, change the gender input from select to radio and add variables for days, names and birth details.

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+var malesNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var dateOfBirth = document.getElementById('date').value;
+var monthOfBirth = document.getElementById()
