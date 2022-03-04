e43812606b8d036311994a524d08219d67588263
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-04 21:18:18 +0300

    create a validation function in js for the input form

--- script/script.js
+++ script/script.js
@@ -3,5 +3,47 @@ var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dateOfBirth = document.getElementById('date').value;
 var monthOfBirth = document.getElementById('month').value;
+var yearOfBirth = document.getElementById('year').value
 var maleGender = document.getElementById('mgender').value;
-var femaleGender = document.getElementById('fgender').value
+var femaleGender = document.getElementById('fgender').value;
+
+function validate(){
+    var dateOfBirth = document.getElementById('date').value;
+    var monthOfBirth = document.getElementById('month').value;
+    var yearOfBirth = document.getElementById('year').value
+    var gender = document.querySelector('input[name="gender"]:checked');
+    // var maleGender = document.getElementById('mgender').value;
+    // var femaleGender = document.getElementById('fgender').value;
+    if(dateOfBirth ===""|| dateOfBirth <=0 || dateOfBirth > 31){
+    alert("Invalid! Please Enter date of birth between the range of 1 - 31");
+    return false;
+    }
+
+    else if(monthOfBirth ===""|| monthOfBirth <=0 || monthOfBirth > 12){
+    alert("Invalid! Please Enter month of birth between the range of 1 - 12");
+    return false;
+    }
+    else if(yearOfBirth ===""){
+        alert("Invalid! Please Enter year of birth.");
+        return false;
+    }
+    else if(gender == null){
+        alert("Please select gender");
+        return false;
+    }
+    else{ return true;}
+}
+
+
+function dayOfTheWeek(CC, YY, MM, DD){
+    return ((((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7);
+}
+var fullDate = [];
+fullDate.push(dateOfBirth, monthOfBirth, yearOfBirth);
+// console.log(fullDate);
+function finder(){
+    // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+    // var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    // var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    console.log(days.length);
+}
