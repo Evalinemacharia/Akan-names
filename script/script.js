8c07a156052bad0cdfd8e11bad372fe4d52efc82
Evaline Wangu <evalinemacharia99@gmail.com>
2022-03-04 16:50:43 +0300

    add Variables for gender to the script file

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
-var malesNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dateOfBirth = document.getElementById('date').value;
-var monthOfBirth = document.getElementById()
+var monthOfBirth = document.getElementById('month').value;
+var maleGender = document.getElementById('mgender').value;
+var femaleGender = document.getElementById('fgender').value
